c653783e01c7590b3d19136d08798c6539e64728
TBerrigan2101 <tberrigan2101@gmail.com>
2022-10-26 20:36:58 +0000

    add code to increment score and incorrect score tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
     
     if (isCorrect) {
         alert("Hey! You got it right! :p");
+        incrementScore();
     } else {
         alert(`Tough luck! You guessed ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -77,11 +79,25 @@ function calculateCorrectAnswer(){
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */ 
+
 function incrementScore(){
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */ 
+
 function incrementWrongAnswer(){
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
